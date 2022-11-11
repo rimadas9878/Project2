a932fcfc4ba63348cfcc14e5bde6e6046a938c8e
Rima Das <rimadas.automationtester@gmail.com>
2022-11-10 18:21:22 -0800

    Website works on localhost and Working Signup page with storing of data in database

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRouters = require('./home-Routes')
 
-
-
+router.use('/api', apiRoutes);
+router.use('/', homeRouters);
 module.exports = router;
