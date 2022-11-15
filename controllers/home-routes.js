5452aae5f1558d66e5db181fd0fb06e6c8ef170a
Rima Das <rimadas.automationtester@gmail.com>
2022-11-14 20:40:52 -0800

    Adding games to the page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -12,64 +12,60 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-router.get("/games",(req,res)=>
- {res.render("gamezone")})
+router.get("/games", (req, res) => { res.render("gamezone") })
 
 
- router.get("/zelda",(req,res)=>
- {res.render('zelda')})
+router.get("/zelda", (req, res) => { res.render('zelda') })
 
- //Route for Super Mario Kart(USA)
- router.get("/SuperMarioKartSNES",(req,res)=>
- {res.render('SuperMarioKartSNES')})
+//Route for Super Mario Kart(USA)
+router.get("/SuperMarioKartSNES", (req, res) => { res.render('SuperMarioKartSNES') })
 
- //Route for Mario Sixty Four
- router.get("/mariosixtyfour",(req,res)=>
- {res.render('mariosixtyfour')})
+//Route for Mario Sixty Four
+router.get("/mariosixtyfour", (req, res) => { res.render('mariosixtyfour') })
 
- //Route for Megaman Sixty Four
- router.get("/megamanSIXTYFOUR",(req,res)=>
- {res.render('megamanSIXTYFOUR')})
+//Route for Megaman Sixty Four
+router.get("/megamanSIXTYFOUR", (req, res) => { res.render('megamanSIXTYFOUR') })
 
-  //Route for Pokemon SILVER
-  router.get("/pokemnonSILVERGB",(req,res)=>
-  {res.render('pokemnonSILVERGB')})
+//Route for Pokemon SILVER
+router.get("/pokemnonSILVERGB", (req, res) => { res.render('pokemnonSILVERGB') })
 
-  //Route for Pokemon BLUE
-  router.get("/pokemonBLUEGB",(req,res)=>
-  {res.render('pokemonBLUEGB')})
+//Route for Pokemon BLUE
+router.get("/pokemonBLUEGB", (req, res) => { res.render('pokemonBLUEGB') })
 
-    //Route for Pokemon YELLOW
-    router.get("/pokemonYELLOWGB",(req,res)=>
-    {res.render('pokemonYELLOWGB')})
+//Route for Pokemon YELLOW
+router.get("/pokemonYELLOWGB", (req, res) => { res.render('pokemonYELLOWGB') })
 
+//Route for South Park
+router.get("/southPark", (req, res) => { res.render('southPark') })
 
-router.get("/bombermanSixtyFourTwo",(req,res)=>
- {res.render('bombermanSixtyFourTwo')})
+//Route for Star Fox
+router.get("/starfoxSNES", (req, res) => { res.render('starfoxSNES') })
 
-router.get("/DiddyKongQuestSNES",(req,res)=>
- {res.render('DiddyKongQuestSNES')})
+//Route for Super Mario World 1
+router.get("/supermarioworldSNES", (req, res) => { res.render('supermarioworldSNES') })
 
-router.get("/FzeroSixtyFour",(req,res)=>
- {res.render('FzeroSixtyFour')})
+//Route for Super Mario World 2
+router.get("/supermarioworldTWOSNES", (req, res) => { res.render('supermarioworldTWOSNES') })
 
-router.get("/GoldenEye",(req,res)=>
- {res.render('GoldenEye')})
+router.get("/bombermanSixtyFourTwo", (req, res) => { res.render('bombermanSixtyFourTwo') })
 
-router.get("/KillerInstinctSixtyFour",(req,res)=>
- {res.render('KillerInstinctSixtyFour')})
+router.get("/DiddyKongQuestSNES", (req, res) => { res.render('DiddyKongQuestSNES') })
 
-router.get("/KobeBryantNBAcourtside",(req,res)=>
- {res.render('KobeBryantNBAcourtside')})
+router.get("/FzeroSixtyFour", (req, res) => { res.render('FzeroSixtyFour') })
 
-router.get("/MaddenNinetynine",(req,res)=>
- {res.render('MaddenNinetynine')})
+router.get("/GoldenEye", (req, res) => { res.render('GoldenEye') })
 
-router.get("/",(req,res) => {
+router.get("/KillerInstinctSixtyFour", (req, res) => { res.render('KillerInstinctSixtyFour') })
+
+router.get("/KobeBryantNBAcourtside", (req, res) => { res.render('KobeBryantNBAcourtside') })
+
+router.get("/MaddenNinetynine", (req, res) => { res.render('MaddenNinetynine') })
+
+router.get("/", (req, res) => {
   res.render("login")
 })
 
-router.get("/signup",(req,res) => {
+router.get("/signup", (req, res) => {
   res.render("signup")
 })
 
@@ -80,11 +76,11 @@ router.get('/', (req, res) => {
   console.log("=====================")
   // If the user is already logged in, redirect the request to another route
   // if (req.session.logged_in) {
-    res.render('games', {
-      title: "GAMEATHON", 
-      user: req.session.username
-    });
-    return;
+  res.render('games', {
+    title: "GAMEATHON",
+    user: req.session.username
+  });
+  return;
   // }
 
   res.redirect('login');
@@ -93,7 +89,7 @@ router.get('/', (req, res) => {
 
 
 module.exports = router;
-// this is wher you set up you res and all that 
+// this is wher you set up you res and all that
 
 // use mini proj for unit 14 to use the syntax for this 
 
